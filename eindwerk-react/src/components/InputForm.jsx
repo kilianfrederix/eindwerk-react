b66b7f892a82f9568ba005bb705354b721da4657
kilian frederix <kilianfrederix@gmail.com>
2024-04-22 16:54:39 +0200

    a

---
+++ eindwerk-react/src/components/InputForm.jsx
@@ -0,0 +1,17 @@
+export default function InputForm() {
+    return (
+        <form className="flex rounded-lg overflow-hidden">
+            <input
+                placeholder="What would you like to pack?"
+                type="text"
+                className="flex-1 p-4 bg-neutral-200 focus:bg-neutral-300/90 transition-all outline-none"
+                defaultValue="Something"
+            />
+            <button
+                type="submit"
+                className="px-4 py-2 bg-blue-500 active:bg-blue-400 text-blue-100 disabled:opacity-50 disabled:cursor-not-allowed transition"
+            >Add item
+            </button>
+        </form>
+    )
+}
--- eindwerk-react/src/components/categoryButtonList/categoryButton.jsx
+++
@@ -1,7 +0,0 @@
-import { Children } from "react"
-
-export default function catergoryList({ children }) {
-    return (
-        pass()
-    )
-}
