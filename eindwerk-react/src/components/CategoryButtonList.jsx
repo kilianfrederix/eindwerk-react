0786f4747280456c12ae51469bba8a3885e617e2
kilian frederix <kilianfrederix@gmail.com>
2024-04-25 13:38:21 +0200

    a

--- eindwerk-react/src/components/CategoryButtonList.jsx
+++ eindwerk-react/src/components/CategoryButtonList.jsx
@@ -1,27 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function CategoryButtonList({ onSelectedCategory }) {
-
+export default function CategoryButtonList({ onSelectedCategory, items }) {
     const [selectedCategory, setSelectedCategory] = useState('');
 
     const handleCategoryClick = (category) => {
-        setSelectedCategory(category)
+        setSelectedCategory(category);
         onSelectedCategory(category);
     };
+
     return (
         <div className="flex gap-2 items-center mb-2">
-        Category:{" "}
-        {['Documents', 'Clothes', 'Camping gear', 'Toiletry'].map((category) => (
-            <button
-                key={category}
-                type="button"
-                className={`transition border ${
-                selectedCategory === category ? 'border-blue-500 bg-blue-500 text-blue-100' : 'border-neutral-200 text-neutral-500'} px-4 py-1 rounded-full`}
-                onClick={() => handleCategoryClick(category)}
-            >
-            {category}
-            </button>
-        ))}
-    </div>
-    )
+            Category:{" "}
+            {['Documents', 'Clothes', 'Camping gear', 'Toiletry'].map((category) => (
+                <button
+                    key={category}
+                    type="button"
+                    className={`transition border ${selectedCategory === category ? 'border-blue-500 bg-blue-500 text-blue-100' : 'border-neutral-200 text-neutral-500'} px-4 py-1 rounded-full`}
+                    onClick={() => handleCategoryClick(category)}
+                >
+                    {category}
+                </button>
+            ))}
+        </div>
+    );
 }
+
